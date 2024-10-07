01370e86d95ad5e4af98c83fd49c5a968cc151f7
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-10-07 11:46:45 +0800

    Added objects/class in app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+//Input
+const person1 ={
+    name:"Ahrdy Jane Desalan",
+    age:19,
+    occupation:"Student",
+    hobbies: "Daydreaming"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
+console.log("Hobbies:"+person1.hobbies);
+
